ed90ced3c505ab6ad5261d18ab63487411678177
Natalee Telles <natalee.telles@gmail.com>
2020-07-10 22:27:04 -0700

    Activity Indicator

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -30,9 +31,18 @@ class Directory extends Component {
                 />
             );
         };
-
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+               </View>
+            );
+        }
         return (
-            <FlatList
+            <FlatList 
                 data={this.props.campsites.campsites}
                 renderItem={renderDirectoryItem}
                 keyExtractor={item => item.id.toString()}
