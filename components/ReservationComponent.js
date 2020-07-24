99b1385aa2d02440a0887f191886c8555f6ebda3
Natalee Telles <natalee.telles@gmail.com>
2020-07-23 23:59:37 -0700

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -34,28 +36,54 @@ class Reservation extends Component {
 
     toggleAlert() {
         const alertText = `
-        Number of Campers: ${this.state.campers}
-        Hike-In? ${this.state.hikeIn}
-        Date:  ${this.state.date}
-        `;
-        Alert.alert(
-        "Begin Search?",
-        alertText,
-        [
-            {
-            text: "Cancel",
-            onPress: () =>
-            console.log("Cancel Pressed"),
-            style: "cancel",
-            onPress: () => this.resetForm()
-            },
-            { text: "OK", onPress: () => 
-            console.log("OK Pressed"),
-            onPress: () => this.resetForm()
-         },
-        ],
-        { cancelable: false }
+            Number of Campers: ${this.state.campers}
+            Hike-In? ${this.state.hikeIn}
+            Date:  ${this.state.date}
+            `;
+            Alert.alert(
+            "Begin Search?",
+            alertText,
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => {
+                        console.log("Cancel Pressed");
+                        this.resetForm();
+                    },
+                    style: 'cancel'
+                },
+                { 
+                    text: 'OK', 
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
+                },
+            ],
+            { cancelable: false }
         );
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
     }
 
     render() {
